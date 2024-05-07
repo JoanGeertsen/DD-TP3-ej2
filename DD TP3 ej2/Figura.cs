08dc368643a349488881509ebcd7e3b7c4989721
Joan Geertsen <JoanGeertsen@outlook.com>
2024-05-07 19:53:28 -0300

    clase Figura terminada

--- DD TP3 ej2/Figura.cs
+++ DD TP3 ej2/Figura.cs
@@ -8,5 +8,26 @@ namespace DD_TP3_ej2
 {
     internal class Figura
     {
+        #region Atributos
+        private int cantVertices; //Dice real pero no tiene sentido
+        #endregion
+
+        #region constructores
+        public Figura(int cantVertices)
+        {
+            this.cantVertices = (cantVertices > 0) ? cantVertices : 0;
+        }
+        #endregion
+
+        #region Consutlas
+        public void perimetro()
+        {
+            //No tiene sentido...
+        }
+        public void area()
+        {
+            //No tiene sentido...
+        }
+        #endregion
     }
 }
