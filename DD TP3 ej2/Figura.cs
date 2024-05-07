4cc5c2b0e215711d150511bef9c372c68bb52be5
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-07 01:00:13 -0300

    Se crearon las clases de las figuras

---
+++ DD TP3 ej2/Figura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD_TP3_ej2
+{
+    internal class Figura
+    {
+    }
+}
