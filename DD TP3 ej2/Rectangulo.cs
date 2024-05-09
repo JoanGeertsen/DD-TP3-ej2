b1371dc498d7203585f2d89dd1fc63d7d2cba9a0
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-08 23:32:32 -0300

    clase Rectangulo completada

--- DD TP3 ej2/Rectangulo.cs
+++ DD TP3 ej2/Rectangulo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.Devices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace DD_TP3_ej2
 {
     internal class Rectangulo: Figura
     {
+        #region Atributos
+        private double basee;
+        private double altura;
+        #endregion
+
+        #region Constructores
+        public Rectangulo(double basee, double altura)
+        {
+            this.basee = (basee > 0) ? basee : 0;
+            this.altura = (altura> 0) ? altura : 0;
+        }
+        #endregion
+
+        #region Property
+        public double Basee
+        {
+            get { return basee; }
+            set { basee = (value > 0) ? value : 0; }
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = (value > 0) ? value : 0; }
+        }
+        #endregion
+
+        #region consultas
+        public double area()
+        {
+            return base * altura;
+        }
+        #endregion
     }
 }
